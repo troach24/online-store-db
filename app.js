82bb330cac2378783682597f88f8cd3d25755b52
Travis Roach <travis.roach24@gmail.com>
2018-06-12 18:14:33 -0600

    add headers

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', '*')
   next()
 })
 

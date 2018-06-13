8dbd210c2b2b1880c4173f89d0013210bbfcadde
Travis Roach <travis.roach24@gmail.com>
2018-06-12 18:39:00 -0600

    add cors

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Origin', '*')
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   res.header('Access-Control-Allow-Origin', '*')
+//   next()
+// })
 
 app.get('/', (req, res) => {
   res.json({

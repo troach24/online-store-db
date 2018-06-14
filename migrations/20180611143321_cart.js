227dd6c60ef02786fa1ae19b18834c56bae125bf
Travis Roach <travis.roach24@gmail.com>
2018-06-14 14:39:17 -0600

    add inventory_id relationshiop to cart table

--- migrations/20180611143321_cart.js
+++ migrations/20180611143321_cart.js
@@ -2,11 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cart', (table) => {
     table.increments();
-    table.integer('inventory_id').unsigned().references('id').inTable('inventory')onDelete("CASCADE");
+    table.integer('inventory_id').references('id').inTable('inventory')
     table.integer('quantity');
   })
+  
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('cart');
+    return knex.schema.dropTable("cart");
 };

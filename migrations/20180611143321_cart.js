667fca8c60e91d0a13e7af3c243eaa446b4912cd
Travis Roach <travis.roach24@gmail.com>
2018-06-17 15:15:39 -0600

    re-seed data

--- migrations/20180611143321_cart.js
+++ migrations/20180611143321_cart.js
@@ -3,6 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('cart', (table) => {
     table.increments();
     table.integer('inventory_id').references('id').inTable('inventory')
+    table.text('cart_item_name')
+    table.float('cart_item_price')
+    table.text('cart_item_description')
+    table.text('cart_item_image_url')  
     table.integer('quantity');
   })
   
--- seeds/02_cart_item.js
+++ seeds/02_cart_item.js
@@ -6,7 +6,11 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           inventory_id: 1,
-          quantity: 1
+          cart_item_name: "Slinky",
+          cart_item_price: 4,
+          cart_item_description: "Classic toy that springs and bounces and even walks down stairs!",
+          cart_item_image_url: "https://shop.australiangeographic.com.au/media/catalog/product/cache/3/image/540x/9df78eab33525d08d6e5fb8d27136e95/A/Y/AYOA-original-metal-slinky-outbox-web.jpg",
+          quantity: 3,
         },
       ]);
     }).then(() => {

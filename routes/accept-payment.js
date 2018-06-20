3c41c6082ee508e95f7dd91d96e3144c78546289
Travis Roach <travis.roach24@gmail.com>
2018-06-19 18:45:45 -0600

    accepting stripe payment

--- routes/accept-payment.js
+++ routes/accept-payment.js
@@ -5,27 +5,26 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/', (req, res) => {
   const token = req.body.stripeToken;
-  const amount = req.body.amount;
+  const amount = req.body.amount * 100;
   console.log(req.body);
   
-  const charge = stripe.charges.create({
+  return stripe.charges.create({
     amount,
     currency: 'USD',
     description: 'One stop shop',
     source: token,
-  });
-  console.log(charge);
-
-  charge.then(result => {
+  })
+  .then(result => {
+    console.log(result);
     res.json(result);
   })
-    .catch(err => {
-      const error = {
-        message: err.message,
-        failure_code: err.code || 500
-      };
-      res.json(error);
-    });
+  .catch(err => {
+    const error = {
+      message: err.message,
+      failure_code: err.code || 500
+    };
+    res.json(error);
+  });
 });
 
 module.exports = router;

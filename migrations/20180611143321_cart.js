ef2f146564a3abc81f07097c753727a92bd89fdd
Travis Roach <travis.roach24@gmail.com>
2018-06-21 08:38:15 -0600

    add 'active' boolean to cart table to track sales history

--- migrations/20180611143321_cart.js
+++ migrations/20180611143321_cart.js
@@ -2,12 +2,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cart', (table) => {
     table.increments();
-    table.integer('inventory_id').references('id').inTable('inventory')
-    table.text('cart_item_name')
-    table.float('cart_item_price')
-    table.text('cart_item_description')
-    table.text('cart_item_image_url')  
+    table.integer('inventory_id').references('id').inTable('inventory');
+    table.text('cart_item_name');
+    table.float('cart_item_price');
+    table.text('cart_item_description');
+    table.text('cart_item_image_url');
     table.integer('quantity');
+    table.boolean('active');
   })
   
 };
--- seeds/02_cart_item.js
+++ seeds/02_cart_item.js
@@ -11,6 +11,7 @@ exports.seed = function(knex, Promise) {
           cart_item_description: "Classic toy that springs and bounces and even walks down stairs!",
           cart_item_image_url: "https://shop.australiangeographic.com.au/media/catalog/product/cache/3/image/540x/9df78eab33525d08d6e5fb8d27136e95/A/Y/AYOA-original-metal-slinky-outbox-web.jpg",
           quantity: 3,
+          active: true,
         },
       ]);
     }).then(() => {
